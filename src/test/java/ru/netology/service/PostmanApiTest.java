f3ea6d9f00784ea231d33b63d0526bf60ea64df9
NadezhdaPogodina <pogodinadia@inbox.ru>
2023-05-17 12:54:34 +0300

    Change src

--- src/test/java/ru/netology/service/PostmanApiTest.java
+++ src/test/java/ru/netology/service/PostmanApiTest.java
@@ -21,7 +21,7 @@ public class PostmanApiTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some values"))
         ;
     }
 
